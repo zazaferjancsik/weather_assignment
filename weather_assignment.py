9f6cd4642bc1f72db97b3e699ebd150eb0cccfde
zaza_ferjancsik <zaza01@ferjancsik.hu>
2024-01-16 14:16:35 +0100

    Added total_yearly, and relative_monthly

--- weather_assignment.py
+++ weather_assignment.py
@@ -16,10 +16,10 @@ for station in stations:                                #creating library for ea
     results[station['Location']]['state'] = station['State']
     results[station['Location']]['total_monthly_precipitation'] = {}
     results[station['Location']]['total_yearly_precipitation'] = 0
-    results[station['Location']]['relative_monthly_precipitation'] = 0
+    results[station['Location']]['relative_monthly_precipitation'] = {}
     results[station['Location']]['relative_yearly_precipitation'] = 0
 
-for data in precipitation:
+for data in precipitation:                          #adding name of the location, so it is easier to sort out each data
     if data['station'] == 'GHCND:USW00093814':
         data['location'] = 'Cincinnati'
     elif data['station'] == 'GHCND:US1WAKG0038':
@@ -30,20 +30,25 @@ for data in precipitation:
         data['location'] = 'San Diego'
     
 
-for data in precipitation:
+for data in precipitation:                              #adding up all monthly precipitation
     if int(data['date'].split('-')[1]) not in results[data['location']]['total_monthly_precipitation']:
         results[data['location']]['total_monthly_precipitation'][int(data['date'].split('-')[1])] = data['value']
     else:
         results[data['location']]['total_monthly_precipitation'][int(data['date'].split('-')[1])] += data['value']
+
+for station in results:                         #adding up the monthly precipitation to get the yearly, for each station
+    for i in range(1, 13):
+        results[station]['total_yearly_precipitation'] += results[station]['total_monthly_precipitation'][i]
+
+for station in results:                     #calculating relative monthly precipitation
+    for i in range(1, 13):
+        results[station]['relative_monthly_precipitation'][i] = results[station]['total_monthly_precipitation'][i] / results[station]['total_yearly_precipitation']
 
 with open('results.json', 'w', encoding='utf-8') as file:               #writing json file
     json.dump(results, file, indent=4)
 with open('precipitation_edited.json', 'w', encoding='utf-8') as file:               #writing json file
     json.dump(precipitation, file, indent=4)
 
-datee = '2020-01-01'
-
-print(int(datee.split('-')[1]))
 
 
 
